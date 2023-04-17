0a72077296e8597d5fd49bc3e780395274b8643e
Mugil99 <124775550+Mugil99@users.noreply.github.com>
2023-04-17 13:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-window.promises = [];
+const promises = [
+  new Promise(resolve => setTimeout(() => resolve(1), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(2), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(3), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(4), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(5), Math.floor(Math.random() * 5000) + 1000))
+];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+Promise.any(promises)
+  .then(result => {
+    const output = document.getElementById('output');
+    output.textContent = `Promise resolved with result: ${result}`;
+  })
+  .catch(error => console.log(error));
